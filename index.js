f5a508aef554d3b87c8f0533f984247fc5012423
MatthewGuyRobin <matthewguyrobinson@gmail.com>
2020-03-27 17:00:46 +0000

    refactored

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-const hbs = require('express-handlebars');
+const hbs = require('express-handlebars')
 const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser');
-
-
 const app = express();
+const getAPI = require("./lib/getMarsWeather")
 
 require('dotenv').config();
 
-const router = require('./routes/router')
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.use('/', router);
 
 app.engine('.hbs', hbs({
     defaultLayout: 'layout',
@@ -24,28 +16,21 @@ app.engine('.hbs', hbs({
 
 app.set('view engine', '.hbs');
 
+app.get("/", async (req, res) => {
+    let data = await getAPI.getMarsWeather();
+    
+    let hiTemp = data[451].AT.mx;
+    let loTemp = data[451].AT.mn;
 
 
 
-// app.post('/', async (req, res) => {
+    res.render('index', {
+        hiTemp,
+        loTemp
+    })
 
-//     let data = await getMarsWeather()
+})
 
-//     console.log(data);
-
-//     let marsHiTemp = '433'.AT.mx;
-
-
-    // let temp = data.main.temp;
-    // let description = data.weather[0].description;
-    // let wind = data.wind.speed;
-
-    // let feelslike = data.list[0].main.feels_like;
-
-    // console.log(data);
-
-//     res.render('index', { marsHiTemp } )
-// 
 
 app.listen(3000, () => {
     console.log('server is listening on 3000')
